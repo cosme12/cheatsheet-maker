0cfac1813c89e941d56016de6a6b08b62a2d7173
cosme12 <aaadiego502@gmail.com>
2017-02-05 14:49:07 -0300

    Language strings updated

--- sheetmaker/language_strings.py
+++ sheetmaker/language_strings.py
@@ -8,8 +8,15 @@ Todo:
 english = {
 	"INTRO_MESSAGE" : "Welcome to CheatSheet Maker",
 	"MENU_MESSAGE" : "Type the number to choose your option.",
-	"CONFIG_SHEET_MESSAGE1" : "Now you will be asked to answer a few simple questions to create the basic layout",
+	"CONFIG_SHEET_MESSAGE1" : "Building the basic layout... answer the next questions.",
 	"CONFIG_SHEET_MESSAGE2" : "How many columns your sheet will have?",
 	"CONFIG_SHEET_MESSAGE3" : "Which color style do you prefer?",
+	"HEADER_MESSAGE" : "Building the header... answer the next questions.",
+	"FOOTER_MESSAGE" : "Building the footer... answer the next questions.",
+	"BLOCK_MESSAGE" : "Building the blocks... answer the next questions.",
+	"BLOCK_ROWS_MESSAGE1" : "Building block with rows... answer the next questions.",
+	"BLOCK_ROWS_MESSAGE2" : "In what main column do you want to build it?",
+	"END_MESSAGE" : "Thanks for using CheatSheet Maker. Feel free to share your ideas at http://github.com/cosme12/cheasheet-maker",
 	"INVALID_INPUT_MESSAGE" : "Invalid input. Try again.",
 }
+
