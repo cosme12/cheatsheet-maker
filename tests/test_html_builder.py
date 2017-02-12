e312bbf49fc9678ffd6542e68d9df666d05df4f1
cosme12 <aaadiego502@gmail.com>
2017-02-12 19:40:48 -0300

    Path changes

--- tests/test_html_builder.py
+++ tests/test_html_builder.py
@@ -4,14 +4,15 @@ print(sys.path)
 import unittest
 
 try:
-    sys.path.insert(0, os.path.abspath('..'))
+    #sys.path.insert(0, os.path.abspath('..'))
     print(sys.path)
     from sheetmaker import html_builder
     from data import test_html_constants
 except:
-    #sys.path.insert(0, os.path.abspath('..'))
+    sys.path.insert(0, os.path.abspath('.'))
+    print(sys.path)
     from sheetmaker import html_builder
-    from tests.data import test_html_constants
+    from data import test_html_constants
 
 
 class HtmlBuilderTestCase(unittest.TestCase):
